e1c061c795246a85d1cb9e3e933c806b9ede135d
Marcelo Luna <ldl_marcelo@hotmail.com>
2023-10-16 20:26:52 -0300

    listar destinos, listar paquetes por destino, realizar reserva, cerrar bd en menu.py, modifico método verificar usuario en personas para que devuelva id y no login para usar en insert de bd

---
+++ Backend/reservas.py
@@ -0,0 +1,8 @@
+import os
+import time
+from destinos import Destinos
+from persona import Persona
+from paquetes import Paquetes
+from mysql_conexion import conectar_bd, cerrar_bd  # Importar la función conectar_bd
+
+verMisReservas()
