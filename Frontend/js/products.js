0674b217c643b125bfdc62a3f521928c5516f5b5
VirginiaFalconi <115593408+VirginiaFalconi@users.noreply.github.com>
2023-10-20 19:52:02 -0300

    Carrito
    Subo el carrito de compras

---
+++ Frontend/js/products.js
@@ -0,0 +1,40 @@
+const destinos = [
+
+    {
+        id: 1,
+        nombre: "Bariloche",
+        precio: 200000,
+        img:
+           "https://i.pinimg.com/originals/43/89/62/438962b8670fbaefad5ac7b44e14370e.jpg",
+        cantidad: 1,
+
+    },
+
+    {
+
+        id: 2,
+        nombre: "Jujuy",
+        precio: 150000,
+        img:
+           "https://i.pinimg.com/originals/97/03/e4/9703e438486bc7b034a01f8add9e9ef8.jpg",
+           cantidad: 1,
+
+    },
+    {
+        id: 3,
+        nombre: "El Calafate",
+        precio: 250000,
+        img:
+           "https://i.pinimg.com/originals/f5/6a/dc/f56adcc0564282bf1654d6a9245dcf34.jpg",
+        cantidad: 1,
+
+    },
+    {
+        id: 4,
+        nombre: "Ushuaia",
+        precio: 300000,
+        img:
+           "https://i.pinimg.com/originals/9c/59/b9/9c59b996da5bb9c418fc6304c5ee2f1e.jpg",
+        cantidad: 1,
+    },
+];
