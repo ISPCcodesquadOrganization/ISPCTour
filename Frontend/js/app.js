042e1e5d3a48d33f95efc7abb902797f1dc077dc
VirginiaFalconi <115593408+VirginiaFalconi@users.noreply.github.com>
2023-10-20 19:38:16 -0300

    cambios
    cambios

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -1,44 +1,44 @@
-//6 aca estoy capturando el id del div shopContent, div padre en el otro paso pongo el div que hice dentro de este
+
 const shopContent = document.getElementById("shopContent");
-//12-aca capturamos el icono carrito 
+
 const verCarrito = document.getElementById("verCarrito");
-//14-aca capturamos el div padre modal-container
+
 const modalContainer = document.getElementById("modal-container");
-//39
+
 const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 
 
 
-let carrito = JSON.parse(localStorage.getItem("carrito")) || []; //2 //get item o tiene algo o es vacio
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []; 
 
-//3 en (destino)puede ir cualquier nombre que querramos
+
 
 destinos.forEach((destino) => {
-  let content = document.createElement("div"); //4-aca estamos creando el div 
-  //4- en content.innerHTML voy a crear los elementos que va dentro del div, aca asignamos contenido HTML
-  content.className = "card-destino"; //9-aca agregamos una clase al div content
+  let content = document.createElement("div"); 
+  
+  content.className = "card-destino"; 
 
   content.innerHTML = `  
      <img src="${destino.img}">
      <h3>${destino.nombre}</h3>
      <p class = "price">${destino.precio} $</p> 
-    `; //9-aca agregamos las clases directamente en la etiqueta
+    `; 
 
   shopContent.append(content);//7
 
 
-  let comprar = document.createElement("button"); //8 creo el elemento boton
-  comprar.innerText = "comprar"; //8-con innerText le agrego el texto al boton
+  let comprar = document.createElement("button"); 
+  comprar.innerText = "comprar"; 
   comprar.className = "comprar-boton-carrito";
 
-  content.append(comprar); //8 con append agrego el boton al contenido de arriba (div)
+  content.append(comprar); 
 
-  comprar.addEventListener("click" , () =>{  //11-aca estamos haciendo que se escuche click en el boton comprar
+  comprar.addEventListener("click" , () =>{  
 
 
 
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === destino.id);//35 este metodo evalua si hay algo repetido
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === destino.id);
 
     if (repeat) {
       carrito.map((prod) => {
@@ -47,19 +47,19 @@ destinos.forEach((destino) => {
         };
       });
     } else{
-      carrito.push({   //-11 la funcion flecha al escuchar el click va a agregar al carrito el producto
-      id : destino.id, // con esto llamamos a todo lo que va a ir al carrito
+      carrito.push({  
+      id : destino.id, 
       img : destino.img,
       nombre : destino.nombre,
       precio : destino.precio,
-      cantidad : destino.cantidad,//34 
+      cantidad : destino.cantidad,
 
       });
 
     }
     console.log(carrito);
 
-    carritoCounter(); //42 llamamos a la funcion para que pinte y muestre el numero del carrito
+    carritoCounter(); 
     saveLocal();
 
   });
@@ -69,19 +69,12 @@ destinos.forEach((destino) => {
 
 
 
-// set item
-
-
 const saveLocal = () => {
 
-  localStorage.setItem("carrito", JSON.stringify(carrito)); // 43 la primer palabra va lo que quieras, y despues la variable que quieran guardar en el localstorage
-  // stringify porque al localstorage solo se le puede mandar strings 
+  localStorage.setItem("carrito", JSON.stringify(carrito)); 
+  
 
 };
 
 
-
-// get item 44
-
-//JSON.parse(localStorage.getItem(carrito)); // hay que parsearla porque es un string, si es una cadena de texto no se puede trabajar con ella en el programa
 
--- Frontend/js/carrito.js
+++ Frontend/js/carrito.js
@@ -1,32 +1,32 @@
-const pintarCarrito = () => { // 26
+const pintarCarrito = () => { 
 
-    modalContainer.innerHTML = ""; //25
+    modalContainer.innerHTML = ""; 
 
-    modalContainer.style.display = "flex";//24 aca le damos otro estilo para que al volver a ver el carrito podamos ver de nuevo los productos
+    modalContainer.style.display = "flex";
 
 
-    const modalHeader = document.createElement("div"); //13 aca creo el div que contiene todo lo del modal
-    modalHeader.className = "modal-header"; //le asigno una clase y en el siguiente creo el contenido HTML
+    const modalHeader = document.createElement("div"); 
+    modalHeader.className = "modal-header"; 
     modalHeader.innerHTML = `
      <h1 class = "modal-header-title">Carrito</h1>
      `;
 
-    modalContainer.append(modalHeader); //15
+    modalContainer.append(modalHeader); 
 
-    const modalButton = document.createElement("h1");//16 el h1 simula a un boton
+    const modalButton = document.createElement("h1");
     modalButton.innerText = "x";
     modalButton.className = "modal-header-button";
 
     modalButton.addEventListener("click", () => {
-        modalContainer.style.display = "none"; //23 esto es para que el modal se cierre style.display es un estilo de css
+        modalContainer.style.display = "none"; 
 
     });
 
-    modalHeader.append(modalButton); //17 
+    modalHeader.append(modalButton); 
 
 
 
-    //18 y 19
+   
     carrito.forEach((destino) => {
         let carritoContent = document.createElement("div");
         carritoContent.className = "modal-content";
@@ -39,15 +39,13 @@ const pintarCarrito = () => { // 26
        <span class = "sumar"> + </span>
        <p>Total: ${destino.cantidad * destino.precio}</p>
        <span class = "delete-product"> ❌ </span>
-       `;//33 cantidad//37 ahi se multiplica para saber el total a pagar
-       //49 le agregamos los span + y -
-       //53
+       `;
 
         modalContainer.append(carritoContent);
 
 
-        let restar = carritoContent.querySelector(".restar");  //50 de carritoContent selecciona la clase restar
-        restar.addEventListener("click", () => {  //51
+        let restar = carritoContent.querySelector(".restar");  
+        restar.addEventListener("click", () => {  
             if(destino.cantidad !== 1){
                 destino.cantidad--;
             };
@@ -58,8 +56,8 @@ const pintarCarrito = () => { // 26
            
         });
 
-        let sumar = carritoContent.querySelector(".sumar");  //50 de carritoContent selecciona la clase sumar
-        sumar.addEventListener("click", () => {  //51
+        let sumar = carritoContent.querySelector(".sumar");  
+        sumar.addEventListener("click", () => {  
             destino.cantidad++;
             
             saveLocal();
@@ -67,72 +65,62 @@ const pintarCarrito = () => { // 26
     
         });
 
-        let eliminar = carritoContent.querySelector(".delete-product"); //53 aca somos mas especificos
+        let eliminar = carritoContent.querySelector(".delete-product"); 
         eliminar.addEventListener("click" , () =>{
 
-            eliminarProducto(destino.id); //53
+            eliminarProducto(destino.id); 
 
         });
 
-        //52 para que las cantidades del carrito esten bien, tambien hay que guardarlas en el localstorage
+        
 
-
-        //console.log(carrito.length);
-
-
-        //let eliminar = document.createElement("span");//28 el span simula un boton
-        //eliminar.innerText = "❌";
-        //eliminar.className = "delete-product";
-        //carritoContent.append(eliminar); //29
-
-
-        eliminar.addEventListener("click", eliminarProducto); //32 el boton tiene que escuchar el click para ejecutar la funcion
-        //32 eliminarProducto sino no borra nada
+        eliminar.addEventListener("click", eliminarProducto); 
+        
 
     });
 
 
-    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0) //20- el cero es de donde arranca el acumulador y en cada vuelta se le suma el precio
-    //38 le agregraos * el.cantidad para que lo multiplique por el precio
-    const totalCompra = document.createElement("div"); //21
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+    
+    const totalCompra = document.createElement("div"); 
     totalCompra.className = "toal-content";
     totalCompra.innerHTML = `Total a pagar: $ ${total}`;
-    modalContainer.append(totalCompra);//22 // el le puso totalBuying
+    modalContainer.append(totalCompra);
 
 };
 
-verCarrito.addEventListener("click", pintarCarrito); //27 aca estamos diciendo que cuando se escuche el click se va a ejecutar la funcion pintarcarrito
+verCarrito.addEventListener("click", pintarCarrito); 
 
 
-const eliminarProducto = (id) => { //30 //53 le pongo el id para conectarlo con el id que toca el boton
-   const foundId = carrito.find((element) => element.id === id); //30 buscame dentro de carrito. (element signiffica cada elemento del carrito), busca ese id 
+const eliminarProducto = (id) => { 
+   const foundId = carrito.find((element) => element.id === id); 
 
    console.log(foundId);
 
-   carrito = carrito.filter((carritoId) => { // carritoId representa todos los elementos del carrito
-     return carritoId !== foundId; // aca retorna todos los elementos del carrito que sean distintos a foundId, que no contenga ese id
+   carrito = carrito.filter((carritoId) => { 
+     return carritoId !== foundId; 
    
     });
-    carritoCounter();//42
+    carritoCounter();
     saveLocal();
-    pintarCarrito(); //31
+    pintarCarrito(); 
 
    
 };
 
 
-const carritoCounter = () => {//40
-    cantidadCarrito.style.display = "block"; // 40 aca se le saca el display none para que pinte el carrito
+const carritoCounter = () => {
+    cantidadCarrito.style.display = "block"; 
 
-    const carritoLength = carrito.length; //45 esto es para guardar la constante en el local storage, para despues eliminar
-    // del localstorage, lo que ya borre del carrito
+    const carritoLength = carrito.length; 
+    
 
-    localStorage.setItem("carritoLength", JSON.stringify(carritoLength)); // 46 aca estamos guardando el dato de la variable de
-    //arriba al localstorage
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength)); 
+   
 
-    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength")); //41 para que muestre el numero
-    //47 obtenemos lo guardado
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength")); 
+    
 };
 
-carritoCounter();// 48 llamamos a la funcion para que cada vez que actualice se muestren los numeros
+carritoCounter();
 
